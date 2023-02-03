11831acb7fec3a9d33210c110351cdd2e6485c70
Juan Diaz <juannavegante2010@gmail.com>
2023-02-02 23:14:30 -0500

    actualización en el js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -38,24 +38,62 @@ function plusSlides(n){
     showSlides(slideIndex += n)
 }
 function currentSlide(n){
-    showSlides(slideIndex = n)
+    showSlides(slideIndex = n);
 }
 function showSlides(n){
     let i;
     let slides = document.querySelectorAll(".mySlides");
-    let quadrates = document.querySelectorAll(".quadrate"); 
+    let quadrates = document.querySelectorAll(".quadrate");
+    
     if(n > slides.length) slideIndex = 1
     if(n < 1) slideIndex = slides.length
     for(i = 0; i < slides.length; i++){
-        slides[i].style.display = "none"
-
+        slides[i].classList.replace("activef","disNone")
     }
     for(i = 0; i < quadrates.length;i++){
         quadrates[i].className = quadrates[i].className.replace("activeC","")
     }
-    slides[slideIndex-1].style.display = "flex";
-    slides[slideIndex-1].style.opacity = "1";
+
+    slides[slideIndex-1].classList.replace("disNone","activef")
     quadrates[slideIndex-1].className += " activeC";
+    
+
+}
+
+
+let slideIndexT = 1;
+function currentSlideT(m){
+    tallas(slideIndexT = m)
+}
+let tallas = function(m){
+    let i;
+    let tallas = document.querySelectorAll(".tallas__container--item") 
+    if(m > tallas.length) slideIndexT = 1
+    if(m < 1) slideIndexT = tallas.length
+    for(i=0; i<tallas.length ;i++){
+        tallas[i].className = tallas[i].className.replace("tallaActive","")
+    }
+    tallas[slideIndexT-1].className += " tallaActive";
+} 
+
+
+
+let slideIndexP = 1
+
+function currentSlideP(o){
+    hover(slideIndexP = o);
+}
+
+
+let hover = function(o){
+    let i;
+    let quadratesP = document.querySelectorAll(".quadrate--p");
+    if(o > quadratesP.length) slideIndexP = 1
+    if(o < 1) slideIndexP = quadratesP.length
+    for(i = 0; i < quadratesP.length; i++){
+        quadratesP[i].classList.remove("disActivP")
+    }
+    quadratesP[slideIndexP-1].classList.add("disActivP");
 }
 
 
